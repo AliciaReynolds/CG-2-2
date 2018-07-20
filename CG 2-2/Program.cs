114e57be0c54b7de04665bfb39cc602e820ce7f0
Alicia Reynolds <alicia.a.wilson@gmail.com>
2018-07-19 22:22:28 -0500

    Add line of text for the area answer

--- CG 2-2/Program.cs
+++ CG 2-2/Program.cs
@@ -6,7 +6,17 @@ namespace CG_2_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is the length of the rectangle?");
+            string length = Console.ReadLine();
+            int l = Convert.ToInt32(length);
+            Console.WriteLine("What is the width of the rectangle?");
+            string width = Console.ReadLine();
+            int w = Convert.ToInt32(width);
+
+            int area = l * w;
+            Console.WriteLine("The area of your rectangle is" + area);
+            Console.ReadLine();
+
         }
     }
 }
